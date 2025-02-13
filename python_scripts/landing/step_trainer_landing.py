eb53adb1af3b513bc499434bcffc0fbf9d6b3fda
Ebenezer Owolabi <seun.data@gmail.com>
2025-02-13 10:50:36 +0000

    updated the DDL files

---
+++ python_scripts/landing/step_trainer_landing.py
@@ -0,0 +1,30 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node step_trainer
+step_trainer_node1739363051967 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lake-house-for-udacity-project/step_trainer/"], "recurse": True}, transformation_ctx="step_trainer_node1739363051967")
+
+# Script generated for node step_trainer landing
+EvaluateDataQuality().process_rows(frame=step_trainer_node1739363051967, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1739358730044", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+step_trainerlanding_node1739363304581 = glueContext.write_dynamic_frame.from_options(frame=step_trainer_node1739363051967, connection_type="s3", format="json", connection_options={"path": "s3://stedi-lake-house-for-udacity-project/step_trainer/landing/", "compression": "gzip", "partitionKeys": []}, transformation_ctx="step_trainerlanding_node1739363304581")
+
+job.commit()
