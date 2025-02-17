65c730f71495c854ce46714f2a91cba67f854931
Ebenezer Owolabi <seun.data@gmail.com>
2025-02-17 13:49:04 +0000

    updated files

--- step_trainer_landing.sql
+++ step_trainer_landing.sql
@@ -1,20 +1,15 @@
-CREATE EXTERNAL TABLE `step_training_landing`(
-  `sensorreadingtime` string COMMENT 'from deserializer', 
-  `serialnumber` string COMMENT 'from deserializer', 
-  `distancefromobject` string COMMENT 'from deserializer')
-ROW FORMAT SERDE 
-  'org.openx.data.jsonserde.JsonSerDe' 
-WITH SERDEPROPERTIES ( 
-  'case.insensitive'='TRUE', 
-  'dots.in.keys'='FALSE', 
-  'ignore.malformed.json'='FALSE', 
-  'mapping'='TRUE') 
-STORED AS INPUTFORMAT 
-  'org.apache.hadoop.mapred.TextInputFormat' 
-OUTPUTFORMAT 
-  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION
-  's3://stedi-lake-house-for-udacity-project/step_trainer/landing'
-TBLPROPERTIES (
-  'classification'='json', 
-  'transient_lastDdlTime'='1739368134')
+CREATE EXTERNAL TABLE IF NOT EXISTS `stediprojecttask_`.`step_trainer_landing` (
+  `sensorreadingtime` string,
+  `serialnumber` string,
+  `distancefromobject` string
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-lake-house-for-udacity-project/step_trainer/landing/'
+TBLPROPERTIES ('classification' = 'json');
